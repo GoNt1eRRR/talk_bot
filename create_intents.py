3218ec33409f4791513555be3ee491bb0a6f793d
GoNt1eR <kuza0998@gmail.com>
2025-08-08 00:31:36 +0500

    add DialogFlow learning script.

---
+++ create_intents.py
@@ -0,0 +1,44 @@
+import json
+from google.cloud import dialogflow
+from environs import env
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    client = dialogflow.IntentsClient()
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=[message_texts])
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+
+def main():
+    env.read_env()
+    PROJECT_ID = env('PROJECT_ID')
+
+    with open('questions.json', 'r', encoding='utf8') as my_file:
+        questions_json = my_file.read()
+
+    questions = json.loads(questions_json)
+
+    work_questions = questions['Устройство на работу']['questions']
+    work_answer = questions['Устройство на работу']['answer']
+
+    create_intent(PROJECT_ID,'Как устроиться к вам на работу', work_questions, work_answer)
+
+
+if __name__ == "__main__":
+    main()
